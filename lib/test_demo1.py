d420cd3772b341d77e7c9f73338f1e074f04996f
Dhruv <dhruvvvbansal@gmail.com>
2021-06-27 21:17:22 +0530

    this first commit with one TCs.

---
+++ lib/test_demo1.py
@@ -0,0 +1,14 @@
+import pytest
+
+@pytest.mark.P1
+def test_TC1P1():
+    a = 10
+    b = 10
+    assert a == b, "a and b is having diffrent values"
+
+
+@pytest.mark.P2
+def test_TC2P2():
+    a = 10
+    b = 10
+    assert a == b, "a and b is having diffrent values"
